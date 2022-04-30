acf0e432444474043c75fa8de60497e4dbc8521e
Jackie Jin <jackie.jin@healthcarelogic.com.au>
2022-04-30 15:47:08 +1000

    dynamicly generate board

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import classnames from 'classnames';
+
+//coefficient is between 1 to 2, it determines the number of initial live cells
+const COEFF = 1.5;
+const ROW_NUM = 10;
+const COL_NUM = 20;
+
+const array = Array.from({ length: ROW_NUM }).map(() => Array.from({ length: COL_NUM }, () => Math.floor(Math.random() * COEFF)));
+console.log(array)
 function App() {
   return (
     <div className="App">
@@ -12,16 +20,24 @@ function App() {
         className='Modal'
         overlayClassName="Modal--Overlay"
       >
-        <div className="Modal--content">
-          {Array.from({ length: 100 }, (_, i) => i).map((num, i) => (
-            <span
-            key={i}
-            className={classnames('Modal--block', {
-              active: num > 5,
-            })}
-          >
-          </span>
-          ))}
+        <div>
+          {array.map((arr: number[]) => {
+            return (
+              <div className="Modal--content">
+                {arr.map((num: number, j: number) => {
+                  return ((
+                    <span
+                      key={j}
+                      className={classnames('Modal--block', {
+                        active: num === 1,
+                      })}
+                    >
+                    </span>
+                  ))
+                })}
+              </div>
+            )
+          })}
         </div>
       </Modal>
     </div>
